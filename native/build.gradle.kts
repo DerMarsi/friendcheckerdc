c422e81169e717fe2d2c8ecb94d532a1064fcf65
Marsi <fa.ke11asd@gmail.com>
2026-01-11 02:39:44 +0100

    fix(build): remove invalid srcDir() call and add index.html

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -51,7 +51,7 @@ android {
     sourceSets {
         named("main") {
             resources {
-                srcDir()
+
                 srcDir(configurations["jsConfiguration"])
             }
         }
